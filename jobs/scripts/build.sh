24c513d673c5eb7efc21ef040002bc7e2693f534
terasako <daigobase123@gmail.com>
2023-12-11 19:03:32 +0900

    test

--- jobs/scripts/build.sh
+++ jobs/scripts/build.sh
@@ -11,5 +11,5 @@ do
   cd "${base_path%/}/`dirname $main_program_path`"
   GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -tags lambda.norpc -o bootstrap main.go
   zip $build_dir.zip bootstrap
-  aws s3 cp $build_dir.zip s3://lambda-terasako --profile example
+  aws s3 cp $build_dir.zip s3://lambda-terasako
 done
