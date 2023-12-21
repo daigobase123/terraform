0e7b6e3aea9adc82a292b8f388ee7236b4ac46a0
terasako <daigobase123@gmail.com>
2023-12-21 18:10:27 +0900

    build

--- jobs/scripts/build.sh
+++ jobs/scripts/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-target_dirs=`find v1 -type d -path "*/functions/*" -not -name "build"`
+
+# gf-aws-lambda-v1-build(python)
+# target_dirs=`find v1 -type d -path "*/functions/"`
+target_dirs=`find v1 -type d -path "*/functions/*"`
 
 for dir in ${target_dirs}
 do
-    if [ ! -d "${dir}/build" ]; then
-        mkdir "${dir}/build"
-    fi
-    zip "${dir}/build/${dir##*/}.zip" -r $dir  
+    zip "${dir}/${dir##*/}.zip" -r $dir  
+
+    echo $dir
 done
